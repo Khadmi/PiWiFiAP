64c6c8076bf1cdec64c29bb0c40f520e04a6e431
Mashkur Khadmi <khadmirits@gmail.com>
2018-12-11 10:36:51 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,6 +90,7 @@ echo "Creating iptabele rules"
 	sudo iptables -A FORWARD -i ppp0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 	sudo iptables -A FORWARD -i wlan0 -o ppp0 -j ACCEPT
 	sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+	sudo iptables-restore < /etc/iptables.ipv4.nat
 
 
 
